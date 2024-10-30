82d7daa07b29930eb24e6a73901708e0d3357940
rukasuleonhart <lucasoliveirasantossoares@gmail.com>
2024-10-30 13:03:07 -0300

    Fase Refactor: Melhorias no código e verificação de entrada

--- media.py
+++ media.py
@@ -1,2 +1,6 @@
 def calcular_media(nota1, nota2, nota3):
+    # Verifica se as notas estão entre 0 e 10
+    notas = [nota1, nota2, nota3]
+    if any(nota < 0 or nota > 10 for nota in notas):
+        raise ValueError("As notas devem estar entre 0 e 10.")
     return (nota1 + nota2 + nota3) / 3
